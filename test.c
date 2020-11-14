d40ae452a039d172d9ef6d7fcfb17821caf2b8e5
Yannis <sauzeauyannis@gmail.com>
2020-11-14 10:32:45 +0100

    Add test file

---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "myassert.h"
+
+#include "Voiture.h"
+#include "Collection.h"
+
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
